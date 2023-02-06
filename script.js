8c49d34b3fa459b7840cd50bf6709e712a23894c
billyx86 <billymk777@gmail.com>
2023-02-06 19:34:26 +0000

    Add playRound and resultOut functions

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -6,10 +9,33 @@ function getComputerChoice() {
     let random = getRandomInt(3);
     
     if(random === 0) {
-        return("Rock");
+        return("rock");
     } else if(random === 1) {
-        return("Paper");
+        return("paper");
     } else if(random === 2) {
-        return("Scissors");
+        return("scissors");
+    }
+}
+
+function resultOut(win, playerSelection, computerSelection) {
+    if(win == true) {
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+    } else {
+        console.log("You lost! " + computerSelection + " beats " + playerSelection);
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection === computerSelection) {
+        playerScore++;
+        computerScore++;
+        console.log("You tied! You both had ", playerSelection);
+    } else if((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
+        playerSelection == "rock" ? (playerScore++, resultOut(1, playerSelection, computerSelection)) : (computerScore++, resultOut(0, playerSelection, computerSelection));
+    } else if((playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors")) {
+        playerSelection == "scissors" ? (playerScore++, resultOut(1, playerSelection, computerSelection)) : (computerScore++, resultOut(0, playerSelection, computerSelection));
+    }  else if((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "paper")) {
+        playerSelection == "paper" ? (playerScore++, resultOut(1, playerSelection, computerSelection)): (computerScore++, resultOut(0, playerSelection, computerSelection));
     }
 }
