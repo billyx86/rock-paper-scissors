81a5b6cbe5847607010f0b4062d03a41cb8da67e
billyx86 <billymk777@gmail.com>
2023-02-07 15:52:03 +0000

    Complete basic javascript

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const playerRock = document.getElementById("rock");
 const playerPaper = document.getElementById("paper");
 const playerScissors = document.getElementById("scissors");
 
+const scoreDiv = document.getElementById("score");
+const resultOut = document.getElementById("result-out")
+
 playerRock.addEventListener('click', function () {
     game("rock");
 });
@@ -19,34 +22,39 @@ playerScissors.addEventListener('click', function () {
 
 // main code
 
+function capitaliseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
 function getComputerChoice() {
     var choices = ["rock", "paper", "scissors"]
     return(choices[Math.floor(Math.random()*choices.length)]);
 }
 
-function resultOut(win, playerSelection, computerSelection) {
+function outputResult(win, playerSelection, computerSelection) {
     if(win == true) {
-        console.log("You win!", playerSelection, "beats", computerSelection);
+        resultOut.textContent = `You win! ${capitaliseFirstLetter(playerSelection)} beats ${computerSelection}.`;
     } else {
-        console.log("You lost!", computerSelection, "beats", playerSelection);
+        resultOut.textContent = `You lost! ${capitaliseFirstLetter(computerSelection)} beats ${playerSelection}.`;
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if(playerSelection === computerSelection) {
-        console.log("You tied! You both had", playerSelection);
+        resultOut.textContent = `You tied! You both had ${playerSelection}.`;
     } else if((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
-        playerSelection == "rock" ? (playerScore++, resultOut(1, playerSelection, computerSelection)) : (computerScore++, resultOut(0, playerSelection, computerSelection));
+        playerSelection == "rock" ? (playerScore++, outputResult(1, playerSelection, computerSelection)) : (computerScore++, outputResult(0, playerSelection, computerSelection));
     } else if((playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors")) {
-        playerSelection == "scissors" ? (playerScore++, resultOut(1, playerSelection, computerSelection)) : (computerScore++, resultOut(0, playerSelection, computerSelection));
+        playerSelection == "scissors" ? (playerScore++, outputResult(1, playerSelection, computerSelection)) : (computerScore++, outputResult(0, playerSelection, computerSelection));
     }  else if((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "paper")) {
-        playerSelection == "paper" ? (playerScore++, resultOut(1, playerSelection, computerSelection)): (computerScore++, resultOut(0, playerSelection, computerSelection));
+        playerSelection == "paper" ? (playerScore++, outputResult(1, playerSelection, computerSelection)): (computerScore++, outputResult(0, playerSelection, computerSelection));
     }
 }
 
 function game(playerSelection) {
     playRound(playerSelection, getComputerChoice());
+    scoreDiv.textContent = `You: ${playerScore}, Computer: ${computerScore}`;
 
     /*if(playerScore > computerScore) {
         console.log("You win with a score of", playerScore);
